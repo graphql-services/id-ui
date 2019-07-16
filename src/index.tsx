c7e3f108ddc8c52a36c33975494cdbee982c50df
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-07-16 12:52:00 +0200

    Add es6-shim

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/ie11';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { App } from './App';
+import 'es6-shim';
 import './env';
 import './index.css';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { App } from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
